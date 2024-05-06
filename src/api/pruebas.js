b89bbd806abfa25deaae66c71253b4ee0686d5d8
brunitaa <bbbb43212345@gmail.com>
2024-05-06 10:36:41 -0400

    Delete

--- src/api/pruebas.js
+++
@@ -1,3 +0,0 @@
-import axios from "./axios";
-
-export const get20Ofertantes = async () => axios.get("/auth/top20Ofertantes");
--- src/config.js
+++
@@ -1,2 +0,0 @@
-export const API_URL =
-  import.meta.env.VITE_API_URL || "https://parkfinder.onrender.com/api/v1";
